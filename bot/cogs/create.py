b6ba507ffb5bd2b34bab387c7384e4e48bef569b
Abdelrahman Sabry <drakth5364@gmail.com>
2024-06-30 23:24:27 +0300

    new file: create.py

---
+++ bot/cogs/create.py
@@ -0,0 +1,16 @@
+from discord.ext import commands
+import discord
+from utils import study
+
+class Create(commands.Cog):
+    """The description for Create goes here."""
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+    
+    @study.command(name='create', help='Create a new study session')
+    async def create(self, ctx: commands.Context, topic: str, start_time: str=None, duration: str=None):
+        pass
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Create(bot))
