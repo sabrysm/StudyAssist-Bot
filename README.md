d96aa843379aeb06f69052ec723c686373f14c98
Abdelrahman Sabry <drakth5364@gmail.com>
2024-06-30 15:46:39 +0300

    Added the following: - prefix variable to config.py - Installation guide in readme - added requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # StudyAssist-Bot
 StudyAssis is a Discord bot designed to facilitate virtual study groups within Discord servers. It provides tools for creating, managing, and enhancing study sessions, fostering collaborative learning environments.
+
+## Installation
+- Setup the bot in Discord Developer portal: https://discord.com/developers/applications
+- Copy the token and paste it in `config.py`
+- Install the dependencies
+```bash
+ > Windows
+ ===========
+ py -m pip install -r requirements.txt
+
+ Linux/Mac
+ ===========
+ python3 -m pip install -r requirements.txt
+ ```
+ - cd bot/
+ - run the bot: `python3 -m bot.py`
--- bot/bot.py
+++ bot/bot.py
@@ -6,7 +6,7 @@ import config
 
 class Bot(commands.Bot):
     def __init__(self, intents: discord.Intents, **kwargs):
-        super().__init__(command_prefix=commands.when_mentioned_or('$'), intents=intents, **kwargs)
+        super().__init__(command_prefix=commands.when_mentioned_or(config.prefix), intents=intents, **kwargs)
 
     async def setup_hook(self):
         for cog in config.cogs:
@@ -14,6 +14,8 @@ class Bot(commands.Bot):
                 await self.load_extension(cog)
             except Exception as exc:
                 print(f'Could not load extension {cog} due to {exc.__class__.__name__}: {exc}')
+        
+        await self.bot.sync()
 
     async def on_ready(self):
         print(f'Logged on as {self.user} (ID: {self.user.id})')
